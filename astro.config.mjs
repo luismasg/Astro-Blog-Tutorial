cf995fe122bbe2a246c7903a187472e8ebf33c49
Luis Manuel Suarez <lsuarez@eastridge.com>
2024-07-04 18:09:49 -0600

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site:"https://bright-pegasus-2be2b3.netlify.app"
+  site: "https://bright-pegasus-2be2b3.netlify.app",
+  integrations: [preact()]
 });
